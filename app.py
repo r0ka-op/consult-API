ae49efcb45ba55acd9e9dcf9a29dc9c852e5230b
r0ka-op <nngfyh@gmail.com>
2024-10-14 20:10:09 +0300

    add CORS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///consults.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
 
 
 class Consult(db.Model):

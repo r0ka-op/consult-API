8c297331d725b3d9d5634f9f9e004c14a096b0cd
r0ka-op <nngfyh@gmail.com>
2024-10-16 21:05:35 +0300

    deleting group and discord keys #bugfix

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from datetime import datetime
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
